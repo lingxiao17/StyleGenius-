0f838bdd45eef69feb023ebd539cef44e9653aeb
Lingchen Xiao <lingchenxiaohohokus@gmail.com>
2025-08-19 22:26:21 -0400

    Initial Commit

---
+++ StyleGenius/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StyleGenius
+//
+//  Created by Lingchen Xiao on 8/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StyleGenius/StyleGeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleGeniusApp.swift
+//  StyleGenius
+//
+//  Created by Lingchen Xiao on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct StyleGeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
